2619fbfaa53e95441e259a21905a12ff2e428cf3
JUYEONGSHIN <hnn09083@naver.com>
2022-05-20 03:03:03 +0900

    first commit

---
+++ .vscode/bg.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+const IMG_NUMBER = 3;
+function handleImageLoad() {
+console.log("finished loading");
+}
+function paintImage(imgNumber) {
+const image = new Image();
+image.src = `images/${imgNumber + 1}.jfif`;
+image.classList.add("bgImage");
+body.prepend(image);
+//image.addEventListener("loadend", handleImageLoad);
+}
+function genRandom() {
+const number = Math.floor(Math.random() * 6);
+return number;
+}
+function init() {
+const randomNumber = genRandom();
+paintImage(randomNumber);
+}
+init();
